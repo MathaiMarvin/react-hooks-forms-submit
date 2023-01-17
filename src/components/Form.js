34c58c7109b67608be446a335a4a1ef0a3973ef2
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2023-01-18 01:55:52 +0300

    Completed Task

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,70 @@
 import React, { useState } from "react";
 
 function Form(props) {
-  const [firstName, setFirstName] = useState("Sylvia");
-  const [lastName, setLastName] = useState("Woods");
+	const [firstName, setFirstName] = React.useState("");
+	const [lastName, setLastName] = React.useState("");
+	const [dataSubmit, setdataSubmit] = React.useState([]);
+	const [errors, setErrors] = React.useState([]);
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
-  }
+	function handleFirstNameChange(event) {
+		setFirstName(event.target.value);
+	}
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
-  }
+	function handleLastNameChange(event) {
+		setLastName(event.target.value);
+	}
+	// Handling of submission of form
+	const handleSubmitForm= (event) => {
+		event.preventDefault();
 
-  return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
-  );
+
+
+		if (firstName.length > 0) {
+			const formData = { firstName, lastName };
+			const nameData = [...dataSubmit, formData];
+			// Create a copy of all the data submitted using spread operator
+
+			setdataSubmit([...dataSubmit, nameData]);
+			setdataSubmit(nameData);
+			console.log(nameData);
+      
+			setFirstName("");
+			setLastName("");
+		} else {
+			setErrors(["First Name is required"]);
+		}
+	};
+
+	const handleSubmissionNames = dataSubmit.map((data, index) => {
+		return (
+			<div key={index}>
+				<p>
+					{`${data.firstName} 
+						${data.lastName}`}
+				</p>
+			</div>
+		);
+	});
+
+	return (
+		<>
+			<form onSubmit={handleSubmitForm}>
+				<input type="text" onChange={handleFirstNameChange} value={firstName} />
+				<input type="text" onChange={handleLastNameChange} value={lastName} />
+				<button type="submit">Submit</button>
+			</form>
+			{/* Rendering of the Error Message */}
+			{errors.length > 0 && (
+				<div>
+					{errors.map((error, index) => (
+						<p key={index}>{error}</p>
+					))}
+				</div>
+			)}
+
+			<h3>Submissions</h3>
+			{handleSubmissionNames}
+		</>
+	);
 }
-
 export default Form;
